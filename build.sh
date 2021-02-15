7d3812f86f2c802c280b0a92cefdbe5ff2a88593
Ricardo Pardini <ricardo@pardini.net>
2021-02-15 14:17:20 +0100

    script for building.

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ else
 fi
 
 echo "Copying static files"...
-cp -vrp ${REPO}/files/* ${OPENWRT}/files/;
+cp -vrp ${REPO}/files/* ${OPENWRT}/files/
 cp -p ${REPO}/config-c7v2 ${OPENWRT}/.config
 cp -p ${REPO}/config-c7v2 ${REPO}/config-c7v2.pre-diff
 
@@ -27,8 +27,13 @@ cd ${OPENWRT}
 make download -j20
 make defconfig
 
+if [[ "$1" == "config" ]]; then
+  echo "Menuconfig as requested..."
+fi
+
 echo "New diff config..."
-scripts/diffconfig.sh > ${REPO}/config-c7v2
+scripts/diffconfig.sh >${REPO}/config-c7v2
+cp .config ${REPO}/config-c7v2-full
 
 echo "Building..."
 make -j9 || make -j1 V=s
@@ -37,4 +42,3 @@ echo "Deploy at TFTP server via sudo..."
 sudo cp -v ${OPENWRT}/bin/targets/ar71xx/generic/openwrt-ar71xx-generic-archer-c7-v2-squashfs-factory.bin /srv/tftp/ArcherC7v2_tp_recovery.bin
 
 echo "Done!"
-
