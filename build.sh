dae4aec0afd99a4292e26dc5b064022ee969f539
Ricardo Pardini <ricardo@pardini.net>
2021-02-16 02:55:19 +0100

    build

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ cp -p ${REPO}/config-c7v2 ${REPO}/config-c7v2.pre-diff
 
 echo "Generate new script for nginx..."
 mkdir -p ${OPENWRT}/files/usr/sbin
-curl -O "${OPENWRT}/files/usr/sbin/main_ingress.sh" "http://192.168.66.67:3000/rpardini/oldskool-rpardini/master/_/scripts/ingress/main_ingress.sh"
+wget -O "${OPENWRT}/files/usr/sbin/main_ingress.sh" "http://192.168.66.67:3000/rpardini/oldskool-rpardini/master/_/scripts/ingress/main_ingress.sh"
 chmod +x "${OPENWRT}/files/usr/sbin/main_ingress.sh"
 
 echo "Prepare for Building..."
